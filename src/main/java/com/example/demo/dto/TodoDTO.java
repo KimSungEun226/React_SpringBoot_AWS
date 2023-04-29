5d8d66ae46dc75f3fc536e953b5f30b49b77220b
KimSungEun226 <tjdt11@naver.com>
2023-04-29 23:38:19 +0900

    실습 컨트롤러, 엔티티, DTO 클래스 추가

---
+++ src/main/java/com/example/demo/dto/TodoDTO.java
@@ -0,0 +1,24 @@
+package com.example.demo.dto;
+
+import com.example.demo.model.TodoEntity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class TodoDTO {
+	private String id; 			// 이 오브젝트의 아이디
+	private String title;		// Todo 타이틀 예) 운동 하기
+	private boolean done;		// true - todo를 완료한 경우(checked)
+	
+	public TodoDTO(final TodoEntity entity) {
+		this.id = entity.getId();
+		this.title = entity.getTitle();
+		this.done = entity.isDone();
+	}
+}
