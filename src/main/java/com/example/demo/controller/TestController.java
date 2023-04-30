ee951c519a1566c71aeb3aadd7b4c1b9fdae9b2a
KimSungEun226 <tjdt11@naver.com>
2023-04-30 22:15:18 +0900

    컨트롤러, 서비스, 레포지토리 생성

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,8 +1,17 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.ResponseDTO;
+import com.example.demo.dto.TestRequestBodyDTO;
 
 @RestController
 @RequestMapping("test") //리소스
@@ -16,6 +25,33 @@ public class TestController {
 	@GetMapping("/testGetMapping")
 	public String testControllerWithPath() {
 		return "Hello World! testGetMapping ";
+	}
+	
+	@GetMapping("/testRequestParam")
+	public String testControllerRequestParam(@RequestParam(required = false) int id) {
+		return "Hello World! ID " + id;
+	}
+	
+	@GetMapping("/testRequestBody")
+	public String testControllerRequestBody(@RequestBody TestRequestBodyDTO testRequestBodyDTO) {
+		return "Hello World! ID " + testRequestBodyDTO.getId() + " Message : " + testRequestBodyDTO.getMessage();
+	}
+	
+	@GetMapping("/testResponseBody")
+	public ResponseDTO<String> testControllerResponseBody() {
+		List<String> list = new ArrayList<>();
+		list.add("Hello World! I'm ResponseDTO");
+		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+		return response;
+	}
+	
+	@GetMapping("/testResponseEntity")
+	public ResponseEntity<?> testControllerReponseEntity() {
+		List<String> list = new ArrayList<String>();
+		list.add("Hello World! I'm ResponseEntity. And you got 400!");
+		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+		//http status를 400로 설정
+		return ResponseEntity.badRequest().body(response);
 	}
 	
 }
