23969f16136f24b6b2bb327f9faf63af979de2b3
sungeun.kim <tjdt11@naver.com>
2023-06-08 13:32:41 +0900

    DBMS 변경(Oracle -> MySQL)

---
+++ src/main/java/com/huibigo/model/BoardEntity.java
@@ -0,0 +1,31 @@
+package com.huibigo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "Board")
+public class BoardEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	private String writer;
+	
+	
+}
