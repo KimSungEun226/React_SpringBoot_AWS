fb74e1e8651b25d1abe9fb9c5d40844b84e26e03
sungeun.kim <tjdt11@naver.com>
2023-06-12 17:51:07 +0900

    게시판 컬럼 수정

--- src/main/java/com/huibigo/model/BoardEntity.java
+++ src/main/java/com/huibigo/model/BoardEntity.java
@@ -1,9 +1,12 @@
 package com.huibigo.model;
 
+import java.time.LocalDateTime;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -22,8 +25,14 @@ public class BoardEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String title;
+	
+	@Lob
+	private String content;
+	
 	private String writer;
 	
-	
+	private LocalDateTime createTime;
+	private LocalDateTime updateTime;
 }
